d279a0e8a98f0e3db81ed7c0fd1bc4b246bd24ab
EstebanPIERRE <101509425+ModimoESTEBAN@users.noreply.github.com>
2023-02-04 11:47:24 +0300

    Add files via upload

---
+++ Arithmetic-Formatter/main.py
@@ -0,0 +1,6 @@
+from arithmetic_formatter import arithmetic_arranger
+
+# receive problems from an input
+#problems = input("Enter problems for formatting: ")
+
+arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "123 + 49"])
---
+++ Time-Formatter/main.py
@@ -0,0 +1,3 @@
+from time_formatter import add_time
+
+add_time("3:00 PM", "3:10")
