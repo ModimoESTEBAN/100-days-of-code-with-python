278cee0002bb7652642e5375a009e781a41d9083
EstebanPIERRE <101509425+ModimoESTEBAN@users.noreply.github.com>
2023-02-08 18:01:17 +0300

    Update polygon.py

--- Polygon-Area-Calculator/polygon.py
+++ Polygon-Area-Calculator/polygon.py
@@ -1,37 +1,44 @@
-import copy
-import random
-# Consider using the modules imported above.
+class Rectangle:
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
 
-class Hat:
-  def __init__(self,**all_item):
-    self.contents=[]
-    for key,value in all_item.items():
-        for itr in range(value):
-          self.contents.append(key)
-  
-  # Function to get random n number of hats from contents
-  def draw(self,amount):
-      draw_list = []
-      if amount >= len(self.contents):
-          return self.contents
-      for i in range(amount):
-          name=self.contents.pop(random.randrange(len(self.contents)))
-          draw_list.append(name)
-      return draw_list
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+    
+    def set_width(self, w):
+        self.width = w
 
+    def set_height(self, h):
+        self.height = h
 
-  
-  # Function to get the probability
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    final_count=0
-    for _ in range(num_experiments):
-      copyhat = copy.deepcopy(hat)
-      temp_list = copyhat.draw(num_balls_drawn)
-      success=True
-      for key,value in expected_balls.items():
-        if temp_list.count(key) < value:
-          success=False
-          break
-      if success:
-        final_count+=1
-    return final_count/num_experiments
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture"
+        
+        pic = "*" * self.width + "\n"
+        pic = pic * self.height
+        return pic
+
+    def get_amount_inside(self, obj):
+        return self.get_area() // obj.get_area()
+
+class Square(Rectangle):
+    def __init__(self, s):
+        super().__init__(s, s)
+
+    def __str__(self):
+        return f"Square(side={self.width})"
+
+    def set_side(self, s):
+        self.width = s
+        self.height = s
