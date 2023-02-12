55fb903ed35935129192a163b82f50bff03e3260
EstebanPIERRE <101509425+ModimoESTEBAN@users.noreply.github.com>
2023-02-12 08:40:32 +0300

    Add files via upload

---
+++ Last-Digit-Of-Large-Number/main.py
@@ -0,0 +1,64 @@
+"""
+Define a function that takes in two non-negative integers 
+�
+a and 
+�
+b and returns the last decimal digit of 
+�
+�
+a 
+b
+ . Note that 
+�
+a and 
+�
+b may be very large!
+
+For example, the last decimal digit of 
+9
+7
+9 
+7
+  is 
+9
+9, since 
+9
+7
+=
+4782969
+9 
+7
+ =4782969. The last decimal digit of 
+(
+2
+200
+)
+2
+300
+(2 
+200
+ ) 
+2 
+300
+ 
+ , which has over 
+1
+0
+92
+10 
+92
+  decimal digits, is 
+6
+6. Also, please take 
+0
+0
+0 
+0
+  to be 
+1
+1.
+"""
+
+import digit
+
+print(digit.last_digit(4,2))
