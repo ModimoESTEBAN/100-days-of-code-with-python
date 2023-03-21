4f2ced3ed1c3f3cba2cf0be87db365bcae54452c
EstebanPIERRE <101509425+ModimoESTEBAN@users.noreply.github.com>
2023-03-21 09:17:27 +0000

    Add files via upload

---
+++ Consonant Value/con.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+print("give me a bottle of rum!")
+def solve(s):
+    count = 0
+    max = 0
+    vowels = ["a", "e", "i", "o", "u"]
+    for letter in s:
+        if letter in vowels:
+            if count > max:
+                max = count
+            count = 0
+        else:
+            count+=int(ord(letter)) - 96
+    return max
